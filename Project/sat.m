351c2d41633c7a789820596bdafa6deb7cc421ae
Pietro1 <pietro.bramante@gmail.com>
2017-07-21 21:50:30 +0200

    Add files via upload

---
+++ Project/sat.m
@@ -0,0 +1,23 @@
+function Y = sat(X)
+%SAT Saturation function 
+%  with unit limits and unit slope
+
+[rows, cols] = size(X);
+Y = zeros(rows, cols);
+
+for i=1:rows
+    for j=1:cols
+        
+        if X(i,j)>1
+           Y(i,j)=1;
+        elseif X(i,j)<-1
+           Y(i,j) = -1;
+        else 
+           Y(i,j) = X(i,j);
+        end
+        
+    end
+end    
+
+end
+
