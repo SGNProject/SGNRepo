413004a30f3ae7cfa9ffd8456232e761e47b2084
Fulvio Di Luzio <fulvio.diluzio@gmail.com>
2017-09-16 00:05:24 +0200

    Magnetic field values
    Added file "magneto field". It contains magnetic field values in Pisa (both in G and nT).

---
+++ Project/Nonlinear Observer/magneto_field.m
@@ -0,0 +1,16 @@
+% Magnetic field values in Pisa
+
+% Date          Declination ( + E  | - W )      Inclination ( + D  | - U )      Horizontal Intensity 	
+% 2017-09-15 	-3.1718°                        58.6830°                        23,834.1 nT              
+
+% North Comp (+ N  | - S)       East Comp (+ E  | - W)          Vertical Comp (+ D  | - U)          Total Field
+%  23,797.6 nT                  -1,318.7 nT                     39,174.0 nT                         45,854.8 nT
+
+% the article requires magnetic filed measured in G (Gauss)
+% 1 G = 100,000 nT 
+
+% 1 G : 100.000 nT = unknown G : known nT   => 
+% unknown G = known nT / 100.00 nT * 1 G
+
+% North Comp (+ N  | - S)       East Comp (+ E  | - W)          Vertical Comp (+ D  | - U)
+% 0,237976 G                    -0,013187 G                        0,39174 G
